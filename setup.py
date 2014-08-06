06f8cc1f43101b66ef1d0f3a341cf27416f3964f
Steve Cassidy <Steve.Cassidy@mq.edu.au>
2014-08-06 10:35:11 +1000

    Change read me to restructured-text for pypi upload

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ from codecs import open
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'readme.md'), encoding='utf-8') as f:
-	long_descripotion = f.read()
+with open(path.join(here, 'readme.rst'), encoding='utf-8') as f:
+	long_description = f.read()
 
 setup(
 	name='pyalveo',
 	version='0.3',
 	description="A Python library for interfacing with the HCSvLab/Alveo API",
-	long_descripotion=long_descripotion,
+	long_description=long_description,
 
 	url='https://github.com/Alveo/pyhcsvlab',
 
-	author='',
-	author_email='',
+	maintainer='Steve Cassidy',
+	maintainer_email='Steve.Cassidy@mq.edu.au',
 	license = 'BSD',
 
 	keywords='alveo HCSvLab python library',
