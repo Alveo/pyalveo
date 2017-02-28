ebc367932343850496a8463e327aaf00082abe07
Michael Bauer <da1da2da3ofme@gmail.com>
2017-02-28 15:17:20 +1100

    Undo New get api key route

--- pyalveo/pyalveo.py
+++ pyalveo/pyalveo.py
@@ -150,7 +150,7 @@ class OAuth2(object):
         try:
             oauth = OAuth2Session(self.client_id,token=self.token,redirect_uri=self.redirect_url,state=self.state)
             
-            response = oauth.get(self.base_url+"account_api_key_oauth",verify=self.verifySSL)
+            response = oauth.get(self.base_url+"account_api_key",verify=self.verifySSL)
             
             if response.status_code != requests.codes.ok:
                 print "Response Code: ",response.status_code," !!"
