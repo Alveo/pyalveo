4805c1e933017928103ee42a0eb6e6a62fb74954
Steve Cassidy <steve.cassidy@mq.edu.au>
2017-10-06 17:26:27 +1100

    Increment version to 1.0.1 to fix bad pypi upload.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
 	name='pyalveo',
-	version='1.0',
+	version='1.0.1',
 	description="A Python library for interfacing with the Alveo API",
 	long_description=long_description,
 
