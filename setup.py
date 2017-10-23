d40eb0f31d6472193e0c876f540aab6487383081
Steve Cassidy <steve.cassidy@mq.edu.au>
2017-10-24 10:01:36 +1100

    Can't get the version number from the package it seems

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup, find_packages
 from os import path
 from codecs import open
 
-from pyalveo import __version__
-
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -11,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
 	name='pyalveo',
-	version=__version__,
+	version='1.0.3',
 	description="A Python library for interfacing with the Alveo API",
 	long_description=long_description,
 
