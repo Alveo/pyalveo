c8e342d97a73547f2cf795b74bb6c771c6c6c11c
Steve Cassidy <steve.cassidy@mq.edu.au>
2018-08-01 17:43:40 +1000

    Update example script and test config.

--- examples/uploadexample.py
+++ examples/uploadexample.py
@@ -12,8 +12,8 @@ for itemuri in client.get_items(collection_uri):
     print("DELETE ITEM:", itemuri)
     try:
         client.delete_item(itemuri)
-    except pyalveo.APIError:
-        pass
+    except pyalveo.APIError as e:
+        print("\tdelete failed")
 
 text = """
 Mrs Campbell was very much frightened and ill for three weeks afterwords but fortunately for me I had not the sense to be frightened. Indeed Mrs Campbell was so much frightened that she told me she had miscarried her twintyeth child but gude forgie  me I think she hardly sticks to the truth in family concerns she makes them all very young and for Mrs McLeod who has three children and is just about to have the fourth she is only twenty one or two and the young ladies three straping queens are from eighteen to fifteen but the youngest may pass for eighteen and the oldest for twinty six. Well we lost sight of St Jago and then we were becalmed for weeks together and but for harpooning sharks and shooting whales I dont know what the gentlemen would have done with themselves and the ladies generally were disputing which of their lords or brothers or lords to be (for there were some matches made up on the way) that had the merit of sending the poor shooten fishes to their long homes - And then, but this is rather a serious story, a young man of the name of Nicholson, a servant of Mrs Campbell, went to sleep in the jolly boat and was struck by the sun.  He died on the ninth day afterwards and was buried on the day after his death. I never never will forget the sound of the deep and hollow plunge when the body was consined  to its fathomless bed of rest. It was a calm day and every wave was as still as death till the mornin after his funeral when all at once there was a breeze got up and in twelve hours we were a hundred miles from poor Nicholson.
@@ -31,7 +31,7 @@ for i in range(3):
 
     itemstart = time.time()
 
-    itemname = "yitem-%d" % i
+    itemname = "item-%d" % i
     meta = {
             'dc:title': 'Test Item %d' % i,
             'dc:creator': 'A. Programmer'
@@ -42,7 +42,9 @@ for i in range(3):
     with tempfile.NamedTemporaryFile() as fd:
         fd.write(b"Hello World")
         fd.flush()
+        docmeta['dc:identifier'] = os.path.basename(fd.name)
         doc_uri = client.add_document(item_uri, os.path.basename(fd.name), docmeta, file=fd.name)
-    print("DOC:", doc_uri)
+        print("DOC:", doc_uri)
 
 print("Overall: ", time.time()-starttime)
+
